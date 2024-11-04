23b852cf43c830bbb22f0c4af1b95cac5263a953
Mikhail Kharlamau <mikhail.kharlamau@gmail.com>
2024-11-04 13:40:46 +0100

    Skusil urobit dodatocne cvicenie, ale nestihol som to

---
+++ Skusobne_cvicenie1/arxiv_bibdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Q=$(echo "$@" | tr ' ' '+')
+Adr="https://arxiv.org/search/?query=$Q&searchtype=all&source=header"
+curl -s -L -A Lynx "$Adr"|
+tr -d '\n' |
+tr '<' '\n' |
+grep -oP 'meta name="citation[^>]+'
